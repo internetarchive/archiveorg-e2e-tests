0a3f871b391844c87ee522d3599cf811d5ba9def
Isa Herico-Velasco <isaherico@gmail.com>
2022-02-28 17:26:56 -0800

    update deps
    add breaking test stub
    
    update dependencies
    
    add test script
    
    update GHA workflow w yarn cache

--- test/your-webcomponent.test.ts
+++ test/your-webcomponent.test.ts
@@ -1,40 +1,8 @@
-import { html, fixture, expect } from '@open-wc/testing';
+import 'testcafe';
 
-import type { YourWebComponent } from '../src/your-webcomponent';
-import '../src/your-webcomponent';
+fixture`Music Player`
+  .page`https://archive.org/details/cd_hanna-barbera-cartoon-sound-fx_william-hanna-joseph-barbera`;
 
-describe('YourWebComponent', () => {
-  it('has a default title "Hey there" and counter 5', async () => {
-    const el = await fixture<YourWebComponent>(
-      html`<your-webcomponent></your-webcomponent>`
-    );
-
-    expect(el.title).to.equal('Hey there');
-    expect(el.counter).to.equal(5);
-  });
-
-  it('increases the counter on button click', async () => {
-    const el = await fixture<YourWebComponent>(
-      html`<your-webcomponent></your-webcomponent>`
-    );
-    el.shadowRoot!.querySelector('button')!.click();
-
-    expect(el.counter).to.equal(6);
-  });
-
-  it('can override the title via attribute', async () => {
-    const el = await fixture<YourWebComponent>(
-      html`<your-webcomponent title="attribute title"></your-webcomponent>`
-    );
-
-    expect(el.title).to.equal('attribute title');
-  });
-
-  it('passes the a11y audit', async () => {
-    const el = await fixture<YourWebComponent>(
-      html`<your-webcomponent></your-webcomponent>`
-    );
-
-    await expect(el).shadowDom.to.be.accessible();
-  });
+test('Page load', async t => {
+  await t.typeText('#theatre-ia', 'John Smith').click('#submit-button');
 });
