b926d45c177c74856a5672a20c9e23c3942d42f8
Isa H Velasco <isaherico@gmail.com>
2022-03-10 20:57:40 -0800

    testcafe skip install

--- test/your-webcomponent.test.ts
+++ test/your-webcomponent.test.ts
@@ -1,8 +1,10 @@
-import 'testcafe';
+import { Selector } from 'testcafe';
 
 fixture`Music Player`
   .page`https://archive.org/details/cd_hanna-barbera-cartoon-sound-fx_william-hanna-joseph-barbera`;
 
 test('Page load', async t => {
-  await t.typeText('#theatre-ia', 'John Smith').click('#submit-button');
+  const linerNotesPlayer = await Selector('.liner-notes-player');
+
+  await t.expect(linerNotesPlayer.exists).eql(true);
 });
