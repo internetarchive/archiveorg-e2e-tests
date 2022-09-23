254fb08bbcca0b2aa2e8f3e574c8800be379c88d
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2022-09-23 12:34:56 -0700

    radio player confirms archive.org uses attribute `skipmusicsections`

--- test/audio/radio-player.test.js
+++ test/audio/radio-player.test.js
@@ -5,24 +5,19 @@ const radioPlayerPage = 'https://archive.org/details/BBC_Radio_2_20180828_010000
 
 fixture`Radio Player`.page`${radioPlayerPage}`;
 
-test('Onload: <radio-player> has `skipmusicsections` attribute', async t => {
+test('Onload: Skips music sections by default', async t => {
+  // archive.org sets <radio-player>'s behaviour to skip music sections
+  // this is by setting `skipmusicsections` attribute on the component
+  // the radio player repo has robust unit & integration tests for its expected behaviour
+  // here, we want to confirm that archive.org's usage applies `skipmusicsections` attr.
+
   const controller = Selector('radio-player-controller').shadowRoot();
+  await t.expect(controller.exists).ok();
 
   // grab <radio-player> inside of controller's shadow DOM
-
-  // try #1 - make it fail
-  // const radioPlayerElement = controller.child('radio-playerz');
-
-  // try #2 - make it succeed
   const radioPlayerElement = controller.child('radio-player');
-
   await t.expect(radioPlayerElement.exists).ok();
 
   // check for attribute
-
-  // try #1 - make it fail
-  // await t.expect(radioPlayerElement.getAttribute('skipmusicsectionszz')).exists;
-
-  // try #2 - make it succeed
   await t.expect(radioPlayerElement.getAttribute('skipmusicsections')).exists;
 });
