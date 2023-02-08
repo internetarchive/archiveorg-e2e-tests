500c3ff0f87dd974d5f57c83cbb9f8c4069c6930
clau <1281581+dualcnhq@users.noreply.github.com>
2023-02-08 15:29:25 +0800

    Update testcafe config

--- .testcaferc.js
+++ .testcaferc.js
@@ -1,17 +1,17 @@
 module.exports = {
-    color: true,
-    quarantineMode: true,
-    concurrency: 2,
-    browsers: [
-        'chrome',
-        'firefox',
-        /* Headless is used in GHA, adding here for visibility */
-        // 'chrome:headless',
-        // 'firefox:headless'
-      ],
-    src: ['tests/**/*test.*'],
-    // reporter: {
-    //   name: 'json',
-    //   output: 'reports/report.json'
-    // },
+  color: true,
+  quarantineMode: true,
+  concurrency: 1,
+  browsers: [
+    'chrome',
+    'firefox',
+    /* Headless is used in GHA, adding here for visibility */
+    // 'chrome:headless',
+    // 'firefox:headless'
+  ],
+  src: ['tests/**/*test.*'],
+  // reporter: {
+  //   name: 'json',
+  //   output: 'reports/report.json'
+  // },
 }
