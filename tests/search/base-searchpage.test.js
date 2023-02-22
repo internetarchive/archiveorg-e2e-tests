8f93c3916f302517530669a92be282d85644b1ee
clau <1281581+dualcnhq@users.noreply.github.com>
2023-02-22 19:25:35 +0800

    Update search tests

--- tests/search/base-searchpage.test.js
+++ tests/search/base-searchpage.test.js
@@ -14,6 +14,30 @@ test('Page load', async t => {
   await t.expect(location.href).eql(url);
   await t.wait(1000); // for load
 
-  await t.expect(search.searchPage.exists).ok();
-  await t.expect(search.searchInput.exists).ok();
+  await t.expect(search.pageSearch.exists).ok();
+  await t.expect(search.inputSearch.exists).ok();
+  await t.expect(search.btnGo.exists).ok();
 });
+
+test('Do search query', async t => {
+  const search = new Search();
+  let location = await getWindowLocation();
+  await t.expect(location.href).eql(url);
+  await t.wait(1000); // for load
+
+  await t.expect(search.inputSearch.exists).ok();
+  await t.expect(search.btnGo.exists).ok();
+  await t.typeText(search.inputSearch, 'cats');
+  await t.expect(search.inputSearch.value).eql('cats');
+
+  await t.pressKey('Enter');
+  await t.wait(6000); // for load
+  location = await getWindowLocation();
+  
+  await t.expect(location.href).eql(`${url}?query=cats`);
+  t.expect(search.collectionBrowser);
+  t.expect(search.collBrowserLeftColumn);
+  t.expect(search.collBrowserRightColumn);
+
+});
+
--- tests/search/models/Search.js
+++ tests/search/models/Search.js
@@ -3,7 +3,18 @@ import { Selector } from 'testcafe';
 /** Model defining Search base elements */
 export default class Search {
   constructor () {
-    this.searchPage = Selector('app-root').shadowRoot().find('search-page');
-    this.searchInput = this.searchPage.shadowRoot().find('collection-search-input');
+    this.pageSearch = Selector('app-root').shadowRoot().find('search-page');
+    this.inputSearch = this.pageSearch
+      .shadowRoot().find('collection-search-input')
+      .shadowRoot().find('ia-clearable-text-input')
+      .shadowRoot().find('#text-input');
+    
+    this.btnGo = this.pageSearch
+      .shadowRoot().find('collection-search-input')
+      .shadowRoot().find('#go-button');
+
+    this.collectionBrowser = this.pageSearch.shadowRoot().find('collection-browser');
+    this.collBrowserLeftColumn = this.collectionBrowser.shadowRoot().find('#left-column');
+    this.collBrowserRightColumn = this.collectionBrowser.shadowRoot().find('#right-column');
   }
 }
