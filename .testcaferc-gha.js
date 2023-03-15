a780968caa619f527ece0ddce502ac2374004097
clau <1281581+dualcnhq@users.noreply.github.com>
2023-03-16 03:53:55 +0800

    Update scripts and file formatting

--- .testcaferc-gha.js
+++ .testcaferc-gha.js
@@ -1,17 +1,17 @@
 module.exports = {
-    skipJsErrors: true,
-    skipUncaughtErrors: true, // https://github.com/DevExpress/testcafe/issues/6807
-    color: true,
-    quarantineMode: true,
-    concurrency: 1,
-    browsers: [
-        /* Headless is used in GHA, adding here for visibility */
-        'chrome:headless',
-        'firefox:headless'
-      ],
-    src: ['tests'],
-    reporter: {
-      name: 'json',
-      output: 'reports/report.json'
-    },
+  skipJsErrors: true,
+  skipUncaughtErrors: true, // https://github.com/DevExpress/testcafe/issues/6807
+  color: true,
+  quarantineMode: true,
+  concurrency: 1,
+  browsers: [
+    /* Headless is used in GHA, adding here for visibility */
+    'chrome:headless',
+    'firefox:headless'
+  ],
+  src: ['tests'],
+  reporter: {
+    name: 'json',
+    output: 'reports/report.json'
+  },
 }
