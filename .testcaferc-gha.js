a9401f657d1628d5027cb9df6b64ee73ed25a1a9
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2023-03-16 03:33:36 +0800

    skip js err (#23)

--- .testcaferc-gha.js
+++ .testcaferc-gha.js
@@ -1,16 +1,15 @@
 module.exports = {
     skipJsErrors: true,
+    skipUncaughtErrors: true, // https://github.com/DevExpress/testcafe/issues/6807
     color: true,
     quarantineMode: true,
     concurrency: 1,
     browsers: [
-        'chrome',
-        'firefox',
         /* Headless is used in GHA, adding here for visibility */
-        // 'chrome:headless',
-        // 'firefox:headless'
+        'chrome:headless',
+        'firefox:headless'
       ],
-    src: ['tests/**/*test.*'],
+    src: ['tests'],
     reporter: {
       name: 'json',
       output: 'reports/report.json'
