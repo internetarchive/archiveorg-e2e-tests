0d6ba09b5c42fc3607f0e54c0f7cfa0c16a62430
clau <1281581+dualcnhq@users.noreply.github.com>
2023-04-07 19:25:30 +0800

    Remove browserstack resolution

--- config/sequential-run.sh
+++ config/sequential-run.sh
@@ -2,7 +2,6 @@
 
 export BROWSERSTACK_PROJECT_NAME="IA e2e test"
 export BROWSERSTACK_BUILD_ID="ia-e2e"
-export BROWSERSTACK_DISPLAY_RESOLUTION="1024x768"
 
 declare -a browsers=( "browserstack:Chrome@84.0:Windows 10" "browserstack:firefox@79.0:Windows 10" "browserstack:edge@84.0:Windows 10" "browserstack:opera@70.0:Windows 10" )
 
--- config/single-run.sh
+++ config/single-run.sh
@@ -2,6 +2,5 @@
 
 export BROWSERSTACK_PROJECT_NAME="IA e2e test"
 export BROWSERSTACK_BUILD_ID="ia-e2e"
-export BROWSERSTACK_DISPLAY_RESOLUTION="1024x768"
 
 ./node_modules/.bin/testcafe "browserstack:ie@11.0:Windows 10" ./tests
