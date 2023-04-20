0de027671ed0e6f11223c662bdf3f043bc87daf2
clau <1281581+dualcnhq@users.noreply.github.com>
2023-04-20 12:40:31 +0800

    Update cron task for testing

--- .testcaferc-gha.js
+++ .testcaferc-gha.js
@@ -1,17 +1,20 @@
+// console.log('prompt: ', process.argv)
+// // console.log('prompt test: ', process.argv[4])
+
 module.exports = {
-    skipJsErrors: true,
-    skipUncaughtErrors: true, // https://github.com/DevExpress/testcafe/issues/6807
-    color: true,
-    quarantineMode: true,
-    concurrency: 1,
-    browsers: [
-        /* Headless is used in GHA, adding here for visibility */
-        'chrome:headless',
-        'firefox:headless'
-      ],
-    src: ['tests'],
-    reporter: {
-      name: 'json',
-      output: 'reports/report.json'
-    },
+  skipJsErrors: true,
+  skipUncaughtErrors: true, // https://github.com/DevExpress/testcafe/issues/6807
+  color: true,
+  quarantineMode: true,
+  concurrency: 1,
+  browsers: [
+    /* Headless is used in GHA, adding here for visibility */
+    'chrome:headless',
+    'firefox:headless'
+  ],
+  src: ['tests'],
+  reporter: {
+    name: 'json',
+    output: 'reports/report.json'
+  },
 }
