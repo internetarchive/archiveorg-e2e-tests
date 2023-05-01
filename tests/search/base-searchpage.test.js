f377d9b070abc3d869b357c413d21d1042ef98d7
banaz <banazsinjary@gmail.com>
2023-05-01 11:49:27 -0700

    updates to get more facets

--- tests/search/base-searchpage.test.js
+++ tests/search/base-searchpage.test.js
@@ -1,4 +1,8 @@
 import { ClientFunction } from 'testcafe';
+// for share e2e test
+// go to facets and select more on search page
+// click to turn on
+// check to see if class is still there 
 
 import Search from './modals/Search';
 
@@ -44,5 +48,30 @@ test('Do search query', async t => {
   await t.expect(search.collBrowserRightColumn.exists).eql(true);
   await t.expect(search.sortFilterBar.exists).eql(true);
   await t.expect(search.infiniteScroller.exists).eql(true);
+});
+
+test('Facets class removal', async t => {
+  const search = new Search();
+  const location = await getWindowLocation();
+  const modalManager = search.appRoot.shadowRoot().find('modal-manager');
+  const shareModalOpen = modalManager.withAttribute('mode','open');
+  const shareModalClosed = modalManager.withAttribute('mode','closed');
+  await t.expect(location.href).eql(url);
+  await t.wait(1000); // for load
+
+  await t.typeText(search.inputSearch, 'hello kitty');
+
+  await t.pressKey('Enter');
+  await t.wait(6000); // for load
+
+  await t.expect(modalManager.exists).ok(); 
+
+  const moreBtn = search.collFacets.shadowRoot().find('more-link');
+  await t.click(moreBtn);
+  await t.wait(1000);
+
+  await t.expect(shareModalOpen.exists).ok();
+
+  
 });
 
