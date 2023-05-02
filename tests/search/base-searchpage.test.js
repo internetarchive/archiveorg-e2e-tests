bca35872051c88b31dffb3062c12c9e0df4f2de5
banaz <banazsinjary@gmail.com>
2023-05-01 20:00:07 -0700

    trying to close modal

--- tests/search/base-searchpage.test.js
+++ tests/search/base-searchpage.test.js
@@ -12,43 +12,43 @@ fixture`Search Page`.page`${url}`;
 
 const getWindowLocation = ClientFunction(() => window.location);
 
-test('Page load', async t => {
-  const search = new Search();
-  const location = await getWindowLocation();
-  await t.expect(location.href).eql(url);
-  await t.wait(1000); // for load
+// test('Page load', async t => {
+//   const search = new Search();
+//   const location = await getWindowLocation();
+//   await t.expect(location.href).eql(url);
+//   await t.wait(1000); // for load
 
-  await t.expect(search.pageSearch.exists).ok();
-  await t.expect(search.inputSearch.exists).ok();
-  await t.expect(search.btnGo.exists).ok();
-  await t.expect(search.actionBar.exists).ok();
-});
+//   await t.expect(search.pageSearch.exists).ok();
+//   await t.expect(search.inputSearch.exists).ok();
+//   await t.expect(search.btnGo.exists).ok();
+//   await t.expect(search.actionBar.exists).ok();
+// });
 
-test('Do search query', async t => {
-  const search = new Search();
-  let location = await getWindowLocation();
-  await t.expect(location.href).eql(url);
-  await t.wait(1000); // for load
+// test('Do search query', async t => {
+//   const search = new Search();
+//   let location = await getWindowLocation();
+//   await t.expect(location.href).eql(url);
+//   await t.wait(1000); // for load
 
-  await t.expect(search.inputSearch.exists).ok();
-  await t.expect(search.btnGo.exists).ok();
-  await t.expect(search.actionBar.exists).ok();
-  await t.typeText(search.inputSearch, 'cats');
-  await t.expect(search.inputSearch.value).eql('cats');
+//   await t.expect(search.inputSearch.exists).ok();
+//   await t.expect(search.btnGo.exists).ok();
+//   await t.expect(search.actionBar.exists).ok();
+//   await t.typeText(search.inputSearch, 'cats');
+//   await t.expect(search.inputSearch.value).eql('cats');
 
-  await t.pressKey('Enter');
-  await t.wait(6000); // for load
-  location = await getWindowLocation();
+//   await t.pressKey('Enter');
+//   await t.wait(6000); // for load
+//   location = await getWindowLocation();
   
-  await t.expect(location.href).eql(`${url}?query=cats`);
-  await t.expect(search.collectionBrowser.exists).eql(true);
-  await t.expect(search.collBrowserLeftColumn.exists).eql(true);
-  await t.expect(search.collFacets.exists).eql(true);
+//   await t.expect(location.href).eql(`${url}?query=cats`);
+//   await t.expect(search.collectionBrowser.exists).eql(true);
+//   await t.expect(search.collBrowserLeftColumn.exists).eql(true);
+//   await t.expect(search.collFacets.exists).eql(true);
 
-  await t.expect(search.collBrowserRightColumn.exists).eql(true);
-  await t.expect(search.sortFilterBar.exists).eql(true);
-  await t.expect(search.infiniteScroller.exists).eql(true);
-});
+//   await t.expect(search.collBrowserRightColumn.exists).eql(true);
+//   await t.expect(search.sortFilterBar.exists).eql(true);
+//   await t.expect(search.infiniteScroller.exists).eql(true);
+// });
 
 test('Facets class removal', async t => {
   const search = new Search();
@@ -74,8 +74,15 @@ test('Facets class removal', async t => {
   await t.click(moreBtn);
   await t.wait(1000);
 
-  // await t.expect(shareModalOpen.exists).ok();
-  // await t.expect(modalManager)
+  await t.expect(shareModalOpen.exists).ok();
+  
+  // const closeModal = modalManager.shadowRoot().find('.close-button');
+  // const closeModal = modalManager.withAttribute('class', 'modal-manager');
+  await t.expect(closeModal.exists).ok();
+  await t.click(closeModal);
+  await t.wait(1000);
+
+  await t.expect(shareModalClosed.exists).ok(); 
 
 
 
