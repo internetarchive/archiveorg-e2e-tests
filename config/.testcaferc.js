e9f156aee340432524dbd22b92cb4b5c49195c13
clau <1281581+dualcnhq@users.noreply.github.com>
2023-05-02 20:16:05 +0800

    Change cron job in 1month

---
+++ config/.testcaferc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    skipJsErrors: true,
+    color: true,
+    quarantineMode: true,
+    concurrency: 1,
+    browsers: [
+        'chrome',
+        'firefox',
+        /* Headless is used in GHA, adding here for visibility */
+        // 'chrome:headless',
+        // 'firefox:headless'
+      ],
+    src: ['tests/**/*test.*'],
+    reporter:{
+      name:'json',
+      output:'reports/report.json'
+    }
+    
+}
